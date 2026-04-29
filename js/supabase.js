bac5310f542fc7ca121f2634b80c35814e121834
kalangodudufla <eduuhd4rk2@gmail.com>
2026-04-28 21:38:27 -0300

    Initialize Supabase client and attach to window
    Fixes the creation of the Supabase client and assigns it to the global window object.

--- js/supabase.js
+++ js/supabase.js
@@ -1,4 +1,7 @@
 const SUPABASE_URL = 'https://hxdzkfypnmzrsdfnicwo.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh4ZHprZnlwbm16cnNkZm5pY3dvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc0MTMxMTcsImV4cCI6MjA5Mjk4OTExN30.P6EsRvrTEKgJQvc97fn3EtPdv7iCi8TRHPnXdC0wbSk';
 
+const { createClient } = supabase
 const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+window.supabase = supabaseClient;
